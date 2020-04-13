06774bae0a4e9e64bda4683c6a84ee0dddabfd63
SERGIO CAMACHO GARCIA <66581034@MX3500006DC0211.iecisa.corp>
2020-04-13 18:50:58 +0200

    correccion extends JpaRepository

--- src/main/java/com/recetas/dao/IIngredienteDao.java
+++ src/main/java/com/recetas/dao/IIngredienteDao.java
@@ -2,13 +2,14 @@ package com.recetas.dao;
 
 import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 
 import com.recetas.entity.Ingrediente;
 import com.recetas.entity.RecetaCocina;
 
 
-public interface IIngredienteDao extends CrudRepository<RecetaCocina, Long> {
+public interface IIngredienteDao extends JpaRepository<RecetaCocina, Long> {
 
 //	public List<Ingrediente> findByRecetaId(Long id);
 	
--- src/main/java/com/recetas/dao/IRecetaCocinaDao.java
+++ src/main/java/com/recetas/dao/IRecetaCocinaDao.java
@@ -2,12 +2,13 @@ package com.recetas.dao;
 
 import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 
 import com.recetas.entity.RecetaCocina;
 
 
-public interface IRecetaCocinaDao extends CrudRepository<RecetaCocina, Long> {
+public interface IRecetaCocinaDao extends JpaRepository<RecetaCocina, Long> {
 
 
 	
